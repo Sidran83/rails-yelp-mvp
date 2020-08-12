914b3c7ad7be3a83ff84d7e87e1b5c4d4b970308
Pierre Deveix <deveixpierre@gmail.com>
2020-08-12 15:45:36 +0200

    yeld done without scss

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  RATING = [0, 1, 2, 3, 4, 5]
 
-  validates :rating, numericality: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: RATING }
+  validates :rating, numericality: true
   validates :content, :rating, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "05-56-65-54-23", category: "french" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "05-56-65-54-23", category: "italian" }
+helios = { name: "Helios", address: "Place d ela comédie", phone_number: "05-56-65-54-23", category: "belgian" }
+moshi =  { name: "Moshi", address: "7 rue de la Regexp", phone_number: "05-56-65-54-23", category: "japanese" }
+pangolin = { name: "Pangolin", address: "18 rue Covid", phone_number: "05-56-65-54-23", category: "chinese" }
+
+[ dishoom, pizza_east, helios, moshi, pangolin ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+s
